add4edbb3cb67d25ee06e346aac09bc034db2453
meisterY88 <gervais.jantou@gmail.com>
2023-03-16 13:05:51 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
  * main - prints a string
  * s : string
  * Description: reads in a string and prints it out to stdout
- * 
  * Return: return value is zero 
  */
 int main(void)
 {    
 	char s[] = "\"Programming is like building a multilingual puzzle";
-
+	
 	puts(s);
       
 	return (0);
