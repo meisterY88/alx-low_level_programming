491a17005daf9c038c952be354e3bf41c84beb1d
meisterY88 <gervais.jantou@gmail.com>
2023-03-16 13:31:43 -0700

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  * Return: return value is zero 
  */
 int main(void)
-{    /**/
+{
 	char s[] = "\"Programming is like building a multilingual puzzle";
-     /**/
+
 	puts(s);
-/**/      
+
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - print the text inside
+ * No parameter
+ * Description: printf command prints simpy prints the text inside double quotes
+ * Return: return value is zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
