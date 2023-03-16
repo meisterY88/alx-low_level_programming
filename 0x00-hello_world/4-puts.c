98e1c5215d5abfbb4376e9b00878371f0985647d
meisterY88 <gervais.jantou@gmail.com>
2023-03-16 12:45:24 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 /*Using puts that prints a string  to stdout*/
-int main()
+int main(void)
 {      /*Declare a character string array to feed in puts*/
 	char s[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(s);
        /*prints out the string s to standard output*/
-	return 0;
+	return (0);
 }
