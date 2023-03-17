3920158dffcbb006823c059f5744a88270922194
meisterY88 <gervais.jantou@gmail.com>
2023-03-17 06:02:59 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - alphabet game
+ * c - character
+ *
+ * Description: prints the alphabet without spaces btw letters
+ * Return: return value is zero
+ */
+int main(void)
+{
+	char c = 'a';
+	for (c='a';c<='z';c++)
+	{printf("%c",c);
+	}
+	printf("\n");
+	
+	return (0);
+}
