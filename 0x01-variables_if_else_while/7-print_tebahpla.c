fbaa15d809a978fd208207ebfe4d89c6d2591cbe
meisterY88 <gervais.jantou@gmail.com>
2023-03-17 08:20:24 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - reverse alphabet
+ * c - chaarcater
+ * 
+ * Description: prints letters a-z in reverse order
+ * Return: return value is zero
+ */
+int main (void)
+{
+	char c = 'z';
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return 0();
+}
