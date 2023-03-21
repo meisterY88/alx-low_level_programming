4af3d77174b0aaf3db6a8347de2da2d50c51a424
meisterY88 <gervais.jantou@gmail.com>
2023-03-21 08:48:45 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,15 @@
  */
 int main(void)
 {
-#include "main.h"
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar(10);
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,3 @@
-putchar(95);
-putchar(112);
-putchar(117);
-putchar(116);
-putchar(99);
-putchar(104);
-putchar(97);
-putchar(114);
-putchar(10);
-
 void print_alphabet(void)
 {
 	int i = 0;
