3af2d0e2d407a8de6aa3d52f9d9a6177b28754da
meisterY88 <gervais.jantou@gmail.com>
2023-03-21 09:51:39 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,10 @@
-void _putchar(int a)
-{
-	putchar(a);
-}
 void print_alphabet(void)
 {
 	int i = 0;
 	for(i = 0; i < 26; i++)
 	{
-		_putchar(97 + i);
+		putchar(97 + i);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
