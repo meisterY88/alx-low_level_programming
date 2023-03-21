7509230cd44ff54914fcfc4aeb4b4679742e5d23
meisterY88 <gervais.jantou@gmail.com>
2023-03-21 07:53:47 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <main.h>
 /**
  * main - print _putchar
  *
@@ -7,15 +6,7 @@
  */
 int main(void)
 {
-	putchar(95);
-	putchar(112);
-	putchar(117);
-	putchar(116);
-	putchar(99);
-	putchar(104);
-	putchar(97);
-	putchar(114);
-	putchar(10);
+#include <main.h>
 
 	return (0);
 }
