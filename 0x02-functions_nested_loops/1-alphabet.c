9ff1192aec01da6fa3b2c2cb15d875665d6e058b
meisterY88 <gervais.jantou@gmail.com>
2023-03-21 11:03:22 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,9 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ *
+ * Return: zero
+ */
 void print_alphabet(void)
 {
 	char c = 'a';
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,2 @@
-#include <unistd.h>
-#include <stdio.h>
 int _putchar(char c);
 void print_alphabet(void);
