6e375fe0e7d7ced575b20d9b2ebbddb9f6a920c2
meisterY88 <gervais.jantou@gmail.com>
2023-03-22 12:42:39 -0700

    98 Battery Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints from a start point to the number 98
+ * @n: integer
+ *
+ * Return: no return value
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	_putchar('\n');
+}
