8d846d23cc997eaa35e8fc57a9d730e80fe1768b
meisterY88 <gervais.jantou@gmail.com>
2023-03-22 13:37:45 -0700

    The World looks like a multiplication table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - prints times tables n
+ * @n: integer not less than 0 and not greater than 15
+ *
+ * Return: no value returned
+ */
+void print_times_table(int n)
+{
+	int i, j = 0;
+	int ij;
+
+	if (n > 0 && n <= 15)
+	{
+		for (i = 0; i < n; i++)
+		{
+		printf("0");
+		for (j = 1; j < n; j++)
+		{
+			ij = i * j;
+
+			if (ij < 10)
+			{
+				printf(",   %d", ij);
+			}
+			else if (ij > 10 && ij < 100)
+			{
+				printf(",  %d", ij);
+			}
+			else if (ij > 99)
+			{
+				printf(", %d", ij);
+			}
+		}
+		printf("\n");
+	}
+	}
+}
