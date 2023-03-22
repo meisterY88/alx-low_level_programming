83eb5436647236214df80d19e0abfce915160026
meisterY88 <gervais.jantou@gmail.com>
2023-03-22 14:49:12 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,13 +8,14 @@ int main(void)
 {
 	int m = 1;
 	int n = 2;
-	int i, sum = 0;
+	int i = 0;
+	long int sum;
 
 	printf("%d, %d", m, n);
 	for (i = 0; i < 48; i++)
 	{
 		sum = m + n;
-		printf(", %d", sum);
+		printf(", %ld", sum);
 		m = n;
 		n = sum;
 	}
